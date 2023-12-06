11a44223eb6cb52a56a691724ec02c168b5c62e6
Sahid <Sahid Munjavar>
2023-12-06 22:14:18 +0000

    Solved easy 2

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,50 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categorys = [];
+
+  for (let i = 0; i < transactions.length; i++) {
+    let isPresent = false;
+
+    if (categorys.length == 0) {
+      categorys.push(transactions[i].category);
+    } else {
+      let isPresent = false;
+      for (let z = 0; z < categorys.length; z++) {
+        if (transactions[i].category === categorys[z]) {
+          isPresent = true;
+          break;
+        }
+      }
+      if (isPresent == false) {
+        categorys.push(transactions[i].category);
+      }
+    }
+  }
+
+  let price = [];
+
+  for (let i = 0; i < categorys.length; i++) {
+    let tprice = 0;
+    for (let z = 0; z < transactions.length; z++) {
+      if (categorys[i] === transactions[z].category) {
+        tprice += transactions[z].price;
+      }
+    }
+    price.push(tprice);
+  }
+
+  let fans = [];
+
+  for (let i = 0; i < categorys.length; i++) {
+    let obj = {};
+    obj["category"] = categorys[i];
+    obj["totalSpent"] = price[i];
+
+    fans.push(obj);
+  }
+
+  return fans;
 }
 
 module.exports = calculateTotalSpentByCategory;
