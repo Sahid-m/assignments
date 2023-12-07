8e1f9c2c4024005ebeea263986544dacc26897e7
Sahid <Sahid Munjavar>
2023-12-07 19:06:52 +0000

    Solved m-3

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,22 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let time = new Date();
+
+  let beforetime = performance.now();
+
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) {
+    sum += i;
+  }
+
+  let nowtime = performance.now();
+
+  let timediff = ((nowtime - beforetime) / 1000).toFixed(5);
+
+  return timediff;
 }
+
+let time = calculateTime(1000000000);
+console.log(time);
